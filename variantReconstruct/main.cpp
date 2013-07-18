c0d297f08a7b0cb9b6f5a0de56f1a285f6098de9
Aditi Gupta <aditi9783@gmail.com>
2013-07-18 15:45:40 -0400

    Initial Commit

---
+++ variantReconstruct/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  variantReconstruct
+//
+//  Created by Aditi Gupta on 7/18/13.
+//  Copyright (c) 2013 Michigan State University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
